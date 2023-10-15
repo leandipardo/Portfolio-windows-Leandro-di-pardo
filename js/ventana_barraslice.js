2b3fee2f17f4e60929353438c795dbc1510e3eff
leandipardo <leandipardo@gmail.com>
2023-10-15 20:47:40 -0300

    v 1.07

--- js/ventana_barraslice.js
+++ js/ventana_barraslice.js
@@ -3,10 +3,10 @@ export default function barraSlice(){
 let $v = d.querySelector(".va-tecnologia-texto"),
 $b = d.querySelector(".va-progres-point"),
 cuenta;
-// d.addEventListener("mouseup",(e)=>{
-//     cuenta = Math.round(Math.abs(($v.clientWidth - $v.scrollLeft / $v.clientWidth) - $v.clientWidth))*25;
-//     $b.style.width=`${(cuenta)}%`;
-// });
+d.addEventListener("mouseup",(e)=>{
+    cuenta = Math.round(Math.abs(($v.clientWidth - $v.scrollLeft / $v.clientWidth) - $v.clientWidth))*25;
+    $b.style.width=`${(cuenta)}%`;
+});
 d.addEventListener("touchmove",(e)=>{
     setTimeout(() => {
         cuenta = Math.round(Math.abs(($v.clientWidth - $v.scrollLeft / $v.clientWidth) - $v.clientWidth))*25;
