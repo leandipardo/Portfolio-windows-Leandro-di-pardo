4734f501326195d6d06e41c32eb0d0d64e863258
leandipardo <leandipardo@gmail.com>
2023-10-17 22:59:51 -0300

    asdasdasd

--- js/darkmode.js
+++ js/darkmode.js
@@ -7,19 +7,20 @@ $moon = d.querySelector(".moon"),
 $sun = d.querySelector(".sun"),
 ubiVideo,
 $body = d.querySelector("body"),
-darkLight = "light";
-const darkModeF = () => {
+darkLight="light";
+function darkModeF(){
     darkLight ="dark";
+    $video.setAttribute("autoplay",`false`);
     ubiVideo = `assets/video/wallpaper dark mode.mp4`;
-    $video.setAttribute("src",`${ubiVideo}`)
-    $video.play();
+    $video.setAttribute("src",`${ubiVideo}`);
+    $video.setAttribute("autoplay",`true`);
     $moon.style.background="var(--dark-mode-hover-plus)";
     $moon.style.transform="translateX(5px)translateY(-3px)"
     $sun.style.border="solid rgba(5, 5, 5, 0.918) 1px;"
     $themeBtn.classList.add("background-buttons");
     $body.classList.toggle("classLight");
 }
-const lightModeF = () =>{
+function lightModeF(){
     darkLight ="light";
     ubiVideo = `assets/video/wallpaper light mode.mp4`;
     $video.setAttribute("src",`${ubiVideo}`)
@@ -33,10 +34,8 @@ d.addEventListener("click", (e)=>{
     if(e.target.matches(btn)|| e.target.matches(`${btn} *`)){
         if(darkLight === "light"){
             darkModeF();
-            darkLight="dark";
         }else if(darkLight === "dark"){
             lightModeF();
-            darkLight="light";
         }
     }
 })
