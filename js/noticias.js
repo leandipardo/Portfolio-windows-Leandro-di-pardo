05d39ca3c49ac25934b82e00cfc28d6cada6a015
leandipardo <leandipardo@gmail.com>
2023-11-28 22:57:37 -0300

    aasdasd

--- js/noticias.js
+++ js/noticias.js
@@ -4,15 +4,15 @@ $fragment = d.createDocumentFragment;
 export default function newsSection(){
     async function getData(){
         try {
-            let res = await fetch("http://api.mediastack.com/v1/news",{
+            let res = await fetch("https://api.mediastack.com/v1/news",{
                 method: 'GET',
                 headers: {
                   'Content-Type': 'application/json',
                 },
                 params: {
                   access_key: 'ACCESS_KEY',
-                  languages: 'fr,-en',
-                  countries: 'ca,fr',
+                  languages: 'es,-en',
+                  countries: 'au',
                   limit: 30,
                   offset: 30,
                 }
