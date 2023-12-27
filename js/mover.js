c7c2bd0ceab6d00e1a84a11da3ada615f6a32cb9
leandipardo <leandipardo@gmail.com>
2023-12-27 20:14:36 -0300

    [Fix]Sistema de movimiento de ventanas

--- js/mover.js
+++ js/mover.js
@@ -1,117 +1,102 @@
 const d = document;
-let inter,
-validacion;
-//función creada por marta para mover ventanas.
-export default function mover(caja){
-    const element = d.querySelectorAll(caja); //selecciona .movible
+let inter;
+
+function breakInterval($ventana) {
+    if (
+        $ventana.right >= window.innerWidth ||
+        $ventana.left < 0 ||
+        $ventana.top < 0 ||
+        $ventana.bottom >= window.innerHeight
+    ) {
+        console.log("Ventana fuera de los límites");
+        // Puedes hacer lo que necesites cuando la ventana esté fuera de los límites aquí
+    }
+}
+
+export default function mover(ventana) {
+    const ventanas = d.querySelectorAll(ventana);
+
     const moveMouse = (e) => {
-            element.forEach(el => { 
-            el.dataset.mouseX = e.clientX; 
+        ventanas.forEach((el) => {
+            el.dataset.mouseX = e.clientX;
             el.dataset.mouseY = e.clientY;
         });
-    }
+    };
+
     d.addEventListener("mousemove", moveMouse);
+
     d.addEventListener("mousedown", (e) => {
         clearInterval(inter);
-        if (e.target.matches(caja)) {
-            let objeto = e.target;
-            let correccion = {
-                x: objeto.dataset.mouseX,
-                y: objeto.dataset.mouseY,
-                oldPosX: 1,
-                oldPosY: 1
-            };
-            let $ventana = objeto.getBoundingClientRect();
-            if (objeto.dataset.transformX !== undefined) {
-                correccion.oldPosX = parseInt(objeto.dataset.transformX);
-                correccion.oldPosY = parseInt(objeto.dataset.transformY);
-            }
-            inter = setInterval(()=>{
-                if($ventana.left <= 0 && correccion.oldPosX + (objeto.dataset.mouseX - correccion.x) <= correccion.oldPosX){
-                    objeto.dataset.transformY = correccion.oldPosY + (objeto.dataset.mouseY - correccion.y);
-                    objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px,${objeto.dataset.transformY}px)`);
-                    return;
-                }if($ventana.right >= window.innerWidth && correccion.oldPosX + (objeto.dataset.mouseX - correccion.x) >= correccion.oldPosX){
-                    objeto.dataset.transformY = correccion.oldPosY + (objeto.dataset.mouseY - correccion.y);
-                    objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px,${objeto.dataset.transformY}px)`);
-                    return;
-                }
-                if(($ventana.y + 100) >= window.innerHeight && (correccion.oldPosY + (objeto.dataset.mouseY - correccion.y) + 50) > correccion.oldPosY){
-                    objeto.dataset.transformX = correccion.oldPosX + (objeto.dataset.mouseX - correccion.x);
-                    objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px, ${objeto.dataset.transformY}px)`);
-                    return;
-                }if($ventana.top <= 0 && correccion.oldPosY + (objeto.dataset.mouseY - correccion.y) < correccion.oldPosY){
-                    objeto.dataset.transformX = correccion.oldPosX + (objeto.dataset.mouseX - correccion.x);
-                    objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px, ${objeto.dataset.transformY}px)`);
-                    return;
-                }else{
-                objeto.dataset.transformX = correccion.oldPosX + (objeto.dataset.mouseX - correccion.x);
-                objeto.dataset.transformY = correccion.oldPosY + (objeto.dataset.mouseY - correccion.y);
-                objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px, ${objeto.dataset.transformY}px);  transition:ease-out .15s`);
-            }
-            }, 15);
+        if (e.target.matches(ventana)) {
+            movimiento(e);
         }
     });
-    d.addEventListener("mouseup", e => {
+
+    d.addEventListener("mouseup", () => {
         clearInterval(inter);
     });
-    // ---------------------touch screen-----------------------
-    
+
     const moveTouch = (e) => {
-        element.forEach(el => { 
+        ventanas.forEach((el) => {
             el.dataset.mouseX = e.touches[0].clientX;
             el.dataset.mouseY = e.touches[0].clientY;
         });
-    }
+    };
+
     d.addEventListener("touchmove", moveTouch);
+
     d.addEventListener("touchstart", (e) => {
         clearInterval(inter);
-        element.forEach(el => {
-            if (el.matches(caja)) {
+        ventanas.forEach((el) => {
+            if (el.matches(ventana)) {
                 el.dataset.mouseX = e.touches[0].clientX;
                 el.dataset.mouseY = e.touches[0].clientY;
             }
         });
-        if (e.target.matches(caja)) {
-            let objeto = e.target;
-            let correccion = {
-                x: (objeto.dataset.mouseX),
-                y: (objeto.dataset.mouseY),
-                oldPosX: 0,
-                oldPosY: 0
-            };
-            if (objeto.dataset.transformX !== undefined) {
-                correccion.oldPosX = parseInt(objeto.dataset.transformX);
-                correccion.oldPosY = parseInt(objeto.dataset.transformY);
-            }
-                inter = setInterval(() => {
-                    let $ventana = objeto.getBoundingClientRect();
-                    if($ventana.left <= 0 && correccion.oldPosX + (objeto.dataset.mouseX - correccion.x) <= correccion.oldPosX){
-                        objeto.dataset.transformY = correccion.oldPosY + (objeto.dataset.mouseY - correccion.y);
-                        objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px,${objeto.dataset.transformY}px)`);
-                        return;
-                    }if($ventana.right >= window.innerWidth && correccion.oldPosX + (objeto.dataset.mouseX - correccion.x) >= correccion.oldPosX){
-                        objeto.dataset.transformY = correccion.oldPosY + (objeto.dataset.mouseY - correccion.y);
-                        objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px,${objeto.dataset.transformY}px)`);
-                        return;
-                    }
-                    if(($ventana.y + 100) >= window.innerHeight && (correccion.oldPosY + (objeto.dataset.mouseY - correccion.y) + 50) > correccion.oldPosY){
-                        objeto.dataset.transformX = correccion.oldPosX + (objeto.dataset.mouseX - correccion.x);
-                        objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px, ${objeto.dataset.transformY}px)`);
-                        return;
-                    }if($ventana.top <= 0 && correccion.oldPosY + (objeto.dataset.mouseY - correccion.y) < correccion.oldPosY){
-                        objeto.dataset.transformX = correccion.oldPosX + (objeto.dataset.mouseX - correccion.x);
-                        objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px, ${objeto.dataset.transformY}px)`);
-                        return;
-                    }else{
-                    objeto.dataset.transformX = correccion.oldPosX + (objeto.dataset.mouseX - correccion.x);
-                    objeto.dataset.transformY = correccion.oldPosY + (objeto.dataset.mouseY - correccion.y);
-                    objeto.setAttribute("style", `transform: translate(${objeto.dataset.transformX}px, ${objeto.dataset.transformY}px); transition:none)`);
-                    }
-                }, 15);
-            }
+        if (e.target.matches(ventana)) {
+            movimiento(e);
+        }
     });
-    d.addEventListener("touchend", e => {
-            clearInterval(inter);
+
+    d.addEventListener("touchend", () => {
+        clearInterval(inter);
     });
+}
+
+function movimiento(e) {
+    let $ventana = e.target.getBoundingClientRect();
+    let objeto = e.target;
+
+    let correccion = {
+        x: objeto.dataset.mouseX,
+        y: objeto.dataset.mouseY,
+        oldPosX: 0,
+        oldPosY: 0,
+    };
+
+    if (objeto.dataset.transformX !== undefined) {
+        correccion.oldPosX = parseInt(objeto.dataset.transformX);
+        correccion.oldPosY = parseInt(objeto.dataset.transformY);
+    }
+
+    inter = setInterval(() => {
+        let newPosX = correccion.oldPosX + (objeto.dataset.mouseX - correccion.x);
+        let newPosY = correccion.oldPosY + (objeto.dataset.mouseY - correccion.y);
+
+        // Comprobar los límites de la pantalla de manera independiente para cada eje
+        if (newPosX >= 0 && newPosX + $ventana.width <= window.innerWidth) {
+            objeto.dataset.transformX = newPosX;
+        }
+
+        if (newPosY >= 0 && newPosY + $ventana.height <= window.innerHeight) {
+            objeto.dataset.transformY = newPosY;
+        }
+
+        objeto.setAttribute(
+            "style",
+            `transform: translate(${objeto.dataset.transformX}px, ${objeto.dataset.transformY}px);  transition: ease-out .1s`
+        );
+
+        breakInterval($ventana);
+    }, 17);
 }
